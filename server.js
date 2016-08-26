d0f7a06e6ccfb71b53055c9ff999286841ec5f0f
Elias Mason <eliasjmason@gmail.com>
2016-08-26 09:36:42 -0700

    Reorganizes files, adjusts server code.

--- server.js
+++ server.js
@@ -1,59 +1,75 @@
 var fs = require('fs');
 const express = require('express'),
-    bodyParser = require('body-parser'),
-    cons = require('consolidate'),
-    hbs = require('hbs');
+	bodyParser = require('body-parser'),
+	hbs = require('hbs'),
     app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
-  extended: false
+	extended: false
 }));
+
+app.use(express.static(__dirname + '/public'));
+
 app.use(function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET,POST');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  next();
+	res.header('Access-Control-Allow-Origin', '*');
+	res.header('Access-Control-Allow-Methods', 'GET,POST');
+	res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+	next();
 });
+
+
 
 app.set('view engine', 'html');
-app.engine('html', require('hbs').__express);
-
 app.set('views', __dirname + '/views');
+
+app.engine('html', require('hbs').__express);
 hbs.registerPartials(__dirname + '/views/partials/');
 
-app.get('/', function(req, res){
-    res.render('index', {title: "Test"});
+app.get('/', function(req, res) {
+	res.render('index', {
+		title: "Address Book"
+	});
 });
 
-app.post('/contacts', function(req, res){
-    res.send(req.body);
-});
-app.get('/contacts', function(req, res){
-    function getData(filename, callback){
-        fs.readFile(filename, function(err, data){
-            if (err) {
-                callback(err);
-                return;
-            }
-            try {
-                callback(null, JSON.parse(data));
-            } catch(exception) {
-                callback(exception);
-            }
-        })
-    }
-
-    getData('contacts.json', function(err, contacts) {
-        if(err) {throw err}
-        res.render('contacts', {contacts: contacts});
+function getData(filename, callback) {
+    fs.readFile(filename, function(err, data) {
+        if (err) {
+            callback(err);
+            return;
+        }
+        try {
+            callback(null, JSON.parse(data));
+        } catch (exception) {
+            callback(exception);
+        }
     })
-});
+}
 
-app.use(express.static('public'));
+app.route('/contacts')
+	.get(function(req, res) {
+		getData('contacts.json', function(err, contacts) {
+			if (err) {
+				throw err
+			}
+			res.render('contacts', {contacts: contacts});
+		})
+	})
+	.post(function(req, res) {
+        getData('contacts.json', function(err, contacts){
+            if (err) {
+				throw err
+			}
+            contacts.push(req.body);
+            var newContacts = JSON.stringify(contacts);
+            fs.writeFileSync('contacts.json', newContacts)
+        });
+        res.redirect('contacts');
+
+    });
 
 app.listen(8081, function() {
-    console.log('Running on port 8081');
+	console.log('Running on port 8081');
 });
 
 exports.app = app;
