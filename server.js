42c44fe2e42b37240ab9cabf9b6842164d91fa73
Elias Mason <eliasjmason@gmail.com>
2016-08-25 19:37:08 -0700

    Finish working contacts view.

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
-require('fs');
-var express = require('express'),
+var fs = require('fs');
+const express = require('express'),
     bodyParser = require('body-parser'),
+    cons = require('consolidate'),
+    hbs = require('hbs');
     app = express();
 
 app.use(bodyParser.json());
@@ -14,15 +16,38 @@ app.use(function(req, res, next) {
   next();
 });
 
+app.set('view engine', 'html');
+app.engine('html', require('hbs').__express);
+
+app.set('views', __dirname + '/views');
+hbs.registerPartials(__dirname + '/views/partials/');
+
 app.get('/', function(req, res){
-    res.sendFile(__dirname + '/public/index.html');
+    res.render('index', {title: "Test"});
 });
 
 app.post('/contacts', function(req, res){
     res.send(req.body);
 });
 app.get('/contacts', function(req, res){
-    res.sendFile(__dirname + 'contacts.json');
+    function getData(filename, callback){
+        fs.readFile(filename, function(err, data){
+            if (err) {
+                callback(err);
+                return;
+            }
+            try {
+                callback(null, JSON.parse(data));
+            } catch(exception) {
+                callback(exception);
+            }
+        })
+    }
+
+    getData('contacts.json', function(err, contacts) {
+        if(err) {throw err}
+        res.render('contacts', {contacts: contacts});
+    })
 });
 
 app.use(express.static('public'));
